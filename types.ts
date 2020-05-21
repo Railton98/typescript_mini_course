3002fb8f7bfb36379f742be9240ea4346dfc8d9e
Railton98 <railtonleal98@gmail.com>
2020-05-21 16:37:03 -0300

    types

---
+++ types.ts
@@ -0,0 +1,53 @@
+// boolean (true / false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message = `foo ${isOpen}`
+
+// number
+let total: number
+total = 0xff0 // hexadecimal
+
+// array (type[])
+let items: string[]
+items = ["foo", "bar"]
+
+// array (generic)
+let values: Array<number>
+values = [1, 2, 3]
+
+// tuple
+let title: [number, string, boolean]
+title = [1, 'foo', true]
+
+// enum
+enum Colors {
+    white = '#fff',
+    black = '#000'
+}
+
+// any (qualquer coisa) NÃO É LEGAL
+let coisa: any
+coisa = 1
+
+// void (vazio)
+function logger(): void {
+    console.log('foo')
+}
+
+// null | undefined
+let foo: null
+type bla = string | undefined
+
+// never
+function error(): never {
+    throw new Error("Error")
+}
+
+// object
+let cart: object
+cart = {
+    key: 'fi'
+}
