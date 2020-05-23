4196ddc66aeccf8b1b2ed84e43c25fe459430aa9
Railton98 <railtonleal98@gmail.com>
2020-05-23 05:05:13 -0300

    my improvements

--- logDetails.ts
+++ logDetails.ts
@@ -1,19 +1,13 @@
 // type alias
 type Uid = number | string
 
-function logDetails(uid: Uid, item: string) {
-    console.log(`A product with ${uid} has a title as ${item}.`)
-}
+const logDetails = (uid: Uid, item: string) => console.log(`A product with ${uid} has a title as ${item}.`)
 
-function logInfo(uid: Uid, user: string) {
-    console.log(`An user with ${uid} has a name as ${user}.`)
-}
+const logInfo = (uid: Uid, user: string) => console.log(`An user with ${uid} has a name as ${user}.`)
 
 type Platform = 'Linux' | 'Mas OS' | 'Windows'| 'BSD'
 
-function renderPlatform(platform:Platform) {
-    return platform
-}
+const renderPlatform = (platform:Platform) =>platform
 
 logDetails(11, "sapato")
 logDetails("11", "camisa")
